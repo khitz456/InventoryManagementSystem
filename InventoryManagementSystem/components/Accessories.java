3a0ed96b4e085964c4d1fadcf8ca5cf5837b1fbb
abacanandrei285-art <abacanandrei285@gmail.com>
2025-11-14 23:47:48 +0800

    Implement Accessories class extending Product

--- InventoryManagementSystem/components/Accessories.java
+++ InventoryManagementSystem/components/Accessories.java
@@ -1,4 +1,20 @@
-package components;
-public t {
-    
-}
+(Accesories)
+ package Component;
+
+public class Accessories extends Product {
+    private String type;
+
+    public Accessories() {}
+
+    public void setType(String type) { this.type=type;}
+
+    public String getType() {return type;}
+
+    @Override
+    public void display(){
+        System.out.print("=== ACCESSORY ITEM ===");
+        System.out.print("Type: " + type);
+        super.display();
+    }
+}    
+

9fd6c8521082c994624741c723034e4900d1ad9e
abacanandrei285-art <abacanandrei285@gmail.com>
2025-11-28 08:09:16 +0800

    Refactor Electronics class package declaration

--- InventoryManagementSystem/components/Electronics.java
+++ InventoryManagementSystem/components/Electronics.java
@@ -1,1 +1,18 @@
-package components;
+package Component;
+
+public class Electronics extends Product {
+    private String brand;
+
+    public Electronics() {}
+
+    public void setBrand(String brand) { this.brand = brand; }
+
+    // GETTER
+    public String getBrand() { return brand; }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("--- ELECTRONIC ITEM ---");
+        System.out.println("Brand: " + brand);
+        super.displayInfo();
+    }
